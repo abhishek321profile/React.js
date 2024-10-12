45b5b75d034b736df64fb745ea63ebcf18645cbc
@abhishekshakya <shyammsingh70@gmail.com>
2024-10-12 09:46:54 +0530

    context api project

---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserContext from './UserContext'
+import PropTypes from 'prop-types'
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
+}
+
+UserContextProvider.propTypes = {
+    children: PropTypes.node
+}
+
+export default UserContextProvider
